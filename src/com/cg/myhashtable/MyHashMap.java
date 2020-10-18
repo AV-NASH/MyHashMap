ebb348b886f4d9957317252168677ae4c6a9f8af
AV-NASH <avinash21213434@gmail.com>
2020-10-18 23:57:11 +0530

    hash map is created with get and add method with test cases

--- src/com/cg/myhashtable/MyHashMap.java
+++ src/com/cg/myhashtable/MyHashMap.java
@@ -1,5 +1,52 @@
 package com.cg.myhashtable;
 
 public class MyHashMap<K,V>  {
+    MapNode head=null;
 
+    public void add(K key, V value){
+        MapNode node=new MapNode(key,value);
+
+        if(head==null)
+            head=node;
+        else{
+            if(getMapNode(key)==null) append(node);
+            else getMapNode(key).value=value;
+        }
+    }
+
+    public void append(MapNode node){
+        MapNode tempNode=head;
+        while(tempNode.next!=null)
+            tempNode=tempNode.next;
+        tempNode.next=node;
+    }
+    public MapNode getMapNode(K searchKey){
+        MapNode searchNode=head;
+        while(searchNode!=null){
+            if(searchNode.key.equals(searchKey)){
+                break;
+            }
+            searchNode=searchNode.next;
+        }
+        return (searchNode==null)?null:searchNode;
+    }
+
+    public V get(K searchKey){
+        return getMapNode(searchKey)==null?null: (V) getMapNode(searchKey).value;
+    }
+
+
+
+
+    public void printList(){
+        if(head==null) System.out.println("HashMap empty");
+        else{
+            MapNode node=head;
+            while(node!=null){
+                System.out.println(node.key+"--"+node.value);
+                node=node.next;
+            }
+
+        }
+    }
 }
--- src/com/cg/myhashtable/MyHashMapTest.java
+++ src/com/cg/myhashtable/MyHashMapTest.java
@@ -6,5 +6,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyHashMapTest {
+    @Test
+    public void whenGivenSentence_storeFrequencyofWords_inKeyValuePair(){
+        MyHashMap<String,Integer> myHashMap=new MyHashMap<String, Integer>();
+        String sampleString="To be or not to be";
+        String[] wordArray=sampleString.toLowerCase().split(" ");
+        for(String word:wordArray){
+            Integer frequency=1;
+            if(myHashMap.get(word)==null){
+                myHashMap.add(word,frequency);
+            }
+            else{
+                frequency= myHashMap.get(word)+frequency;
+                myHashMap.add(word,frequency);
+            }
+        }
+        Integer expected=2;
+        Assert.assertEquals(expected,myHashMap.get("to"));
 
+    }
 }
